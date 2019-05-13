62693e1a0b3df740553a45264f337d260e4fe89e
JennicaStiehl <jennica.stiehl@gmail.com>
2019-05-13 11:06:41 -0600

    adds model test for fuel stations

---
+++ spec/facades/stations_facade_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+describe StationsFacade, type: :model do
+  describe 'instance methods' do
+    it 'can get station data' do
+
+    end
+  end
+end
--- spec/models/fuel_station_spec.rb
+++ spec/models/fuel_station_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 describe FuelStation, type: :model do
   describe 'instance methods' do
     it 'can get station data' do
-
-      conn = Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest?api_key=yxLmV0MF2MDdepeWj58PDNqY9fw47hScScgUTulc&location=80206&format=json&fuel_type=ELEC,LPG&access=public&limit=15")
-      response = conn.get
-      results = JSON.parse(response.body, symbolize_names: true)
-      data = results[:fuel_stations]
-      obj = data.map do |fuel_station_data|
-        FuelStation.new(fuel_station_data)
-      end
-      binding.pry
-      expect(obj.first.name).to eq("PUBLIC STATIONS")
+      attributes = {
+       name: "PUBLIC STATIONS",
+       address: "123 thatsthe way",
+       fuel_type: "bghj",
+   }
+      station = FuelStation.new(attributes)
+      expect(station.name).to eq("PUBLIC STATIONS")
+      expect(station.address).to eq("123 thatsthe way")
+      expect(station.fuel_type).to eq("bghj")
     end
   end
 end
